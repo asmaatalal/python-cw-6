f1103b52b92c1e6e65fd3cca9af45c32a5073ac6
asmaatalal <142660888+asmaatalal@users.noreply.github.com>
2023-09-07 10:15:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 # write your code here
+person = {
+    'name': "asmaa" ,
+    'age' : 16 ,
+    'hobbies' : ['drawing' , 'swimming' , 'reading']
+}
+
+print(person.get('name'))
+print(person.get('age'))
+
+person['age']= 17
+print(person)
+
+person['country'] = 'syria'
+print(person)
+print(len(person))
+
+person['hobbies'].append('skating')
+print(person)
+
+def check_hobbies(person) :
+    if len(person['hobbies']) >= 3 :
+        print('wow you are amazing !!')
+    else :
+        print('try to do something new !! ')
+check_hobbies(person)
